8f010b03f5fc1186e2d6160175dcfd05ba4b0d96
paulojsfilho <paulojsfilho@gmail.com>
2015-05-11 00:50:51 -0300

    conexão 2

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
- $connect = mysqli_connect("br-cdbr-azure-south-a.cloudapp.net", "b52f673b9e8d7a", "82b32b8c", "paulojosebd");
+ $connect = mysqli_connect("http://br-cdbr-azure-south-a.cloudapp.net/", "b52f673b9e8d7a", "82b32b8c", "paulojosebd");
        if(mysqli_connect_errno()) {
           echo "<h1>Falha na conexão.  Entre em contato com o Administrador!</h1>";
            echo mysqli_connect_error();
